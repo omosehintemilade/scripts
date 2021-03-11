1ae0665465f216329b8e59175e16caf712df8d52
frontdevguy <ogunbojoshua@gmail.com>
2021-03-11 19:45:03 +0100

    Implement payment with stripe

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,6 @@
 var CONSTANTS = {
-    baseUrl: "https://aluuka-backend.herokuapp.com"
-    //baseUrl: "http://localhost:5500"
-}
+	baseUrl: 'https://aluuka-graph.herokuapp.com',
+	stripePublicKey:
+		'pk_test_51IToLLFdn8eWHl79XsOXtEowMbLpBEQnDdGx6MnzHEG14U7lbEbCddhJkXspSkJxUMOqUJCoNBPkyg0ghV1Xok5F000zCJ3K1F'
+	//baseUrl: "http://localhost:5500"
+};
