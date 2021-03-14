6a471985641757700d9b7db092dd63445e87266e
Olamigoke Philip <olamigokayphils@gmail.com>
2021-03-14 14:42:43 +0100

    update base url

--- js/constants.js
+++ js/constants.js
@@ -2,9 +2,5 @@ var CONSTANTS = {
 	//baseUrl: 'https://aluuka-graph.herokuapp.com',
 	stripePublicKey:
 		'pk_test_51IToLLFdn8eWHl79XsOXtEowMbLpBEQnDdGx6MnzHEG14U7lbEbCddhJkXspSkJxUMOqUJCoNBPkyg0ghV1Xok5F000zCJ3K1F',
-<<<<<<< HEAD
-	baseUrl: "http://localhost:5500"
-=======
 	//baseUrl: "http://localhost:5500"
->>>>>>> 669adcba1d51a5ef67ef2725144eb5dc2349be44
 };
