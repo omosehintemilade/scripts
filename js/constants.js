b0e5c0def671c9f1a39b5d1827e7292a195a358d
Olamigoke Philip <olamigokayphils@gmail.com>
2021-03-15 10:36:26 +0100

    :zap: complete dashboard filtering options

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 var CONSTANTS = {
-	baseUrl: 'https://aluuka-graph.herokuapp.com',
+	//baseUrl: 'https://aluuka-graph.herokuapp.com',
 	stripePublicKey:
 		'pk_test_51IToLLFdn8eWHl79XsOXtEowMbLpBEQnDdGx6MnzHEG14U7lbEbCddhJkXspSkJxUMOqUJCoNBPkyg0ghV1Xok5F000zCJ3K1F',
-	//baseUrl: "http://localhost:5500"
+	baseUrl: "http://localhost:5500"
 };
