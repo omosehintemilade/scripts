a0d3537566ec33db0c6a17ea3b302be4925d84c9
Olamigoke Philip <olamigokayphils@gmail.com>
2021-03-21 21:36:44 +0100

    Generic update  with fork

--- js/reset-password.js
+++ js/reset-password.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
-  var errorMessage = $(".error-message");
 
+  $("body").prepend('<div class="error-message"></div>');
+    
+  $(".sign-out").click(function () {
+    localStorage.clear();
+    var loc = `${$(location).attr("origin")}/health-care-provider/login.html`;
+    $(location).attr("href", loc);
+  });
+
+  var errorMessage = $(".error-message");
+  
   function getUrlVars() {
     var vars = [],
       hash;
