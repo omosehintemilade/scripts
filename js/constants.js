3f85e9ce1604c2686157f24938fb0b93bb57b0b5
Olamigoke Philip <olamigokayphils@gmail.com>
2021-03-22 14:35:27 +0100

    update stripe api keys

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,5 @@
 var CONSTANTS = {
-	//baseUrl: 'https://aluuka-graph.herokuapp.com',
-	stripePublicKey:
-		'pk_test_51IToLLFdn8eWHl79XsOXtEowMbLpBEQnDdGx6MnzHEG14U7lbEbCddhJkXspSkJxUMOqUJCoNBPkyg0ghV1Xok5F000zCJ3K1F',
-	baseUrl: "http://localhost:5500"
+	baseUrl: 'https://aluuka-graph.herokuapp.com',
+	stripePublicKey: "pk_test_51I6kgXAhVLlRf9SpVA2Vg0K7fp83R0vm0lnOzT58TIJNqxi34NYlzIN4C9qYJVVWURaD1Y4Z14MomE3EnLu0MjeG00xAckuDw5",
+	//baseUrl: "http://localhost:5500"
 };
